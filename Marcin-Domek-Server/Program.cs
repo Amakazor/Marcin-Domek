7db3d48662201bdd7fa8506d5e6073c2b00f6bc9
amakazor <amakazor@gmail.com>
2020-11-28 19:20:54 +0100

    init

---
+++ Marcin-Domek-Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Marcin_Domek_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
